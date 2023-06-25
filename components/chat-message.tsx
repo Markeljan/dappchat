8c9751a0a77e0ebf8a1bb0c11089713a77fedeeb
Dimitri <dimitri4d@gmail.com>
2023-06-25 08:23:48 -0400

    Feat/UI copy updates (#4)
    * Update styles & copy
    
    * pre prompts

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -13,7 +13,9 @@ export interface ChatMessageProps {
   message: Message
 }
 
-export function ChatMessage({ message, ...props }: ChatMessageProps) {
+export function ChatMessage({ message, ...props }: ChatMessageProps | null) {
+  if (message?.role === 'system') return null
+
   return (
     <div
       className={cn('group relative mb-4 flex items-start md:-ml-12')}
