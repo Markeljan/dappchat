a2e4d13fcc4c29008b6bb028a67680697d7770c2
Markeljan Sokoli <Markeljan19@gmail.com>
2023-06-25 10:00:37 -0400

    sismo test

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,16 +1,22 @@
 import { auth } from '@/auth'
 import { LoginButton } from '@/components/login-button'
+import SismoButton from '@/components/sismo-button'
 import { redirect } from 'next/navigation'
 
-export default async function SignInPage() {
-  const session = await auth()
+export default function SignInPage() {
+  const { data: session, error } = auth();
+
   // redirect to home if user is already logged in
   if (session?.user) {
     redirect('/')
   }
+
   return (
     <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
+      {/* Your original LoginButton */}
       <LoginButton />
+      {/* Your SismoConnectButton */}
+      <SismoButton />
     </div>
   )
 }
