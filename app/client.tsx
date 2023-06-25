f8eda9939ffd6fcc5039a05cecf92c6bc08ed020
Markeljan Sokoli <Markeljan19@gmail.com>
2023-06-25 01:44:29 -0400

    fix wagmi

--- app/client.tsx
+++ app/client.tsx
@@ -2,8 +2,7 @@
 
 import { FC, PropsWithChildren } from 'react'
 import { gnosis } from 'viem/chains'
-import { createPublicClient, http } from 'viem'
-import { WagmiConfig, createConfig, configureChains, ChainProviderFn } from 'wagmi'
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { publicProvider } from 'wagmi/providers/public';
@@ -11,17 +10,6 @@ import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 
 const QUICKNODE_API_KEY = process.env.NEXT_PUBLIC_QUICKNODE_API_KEY || ''
 const WALLETCONNECT_ID = process.env.NEXT_PUBLIC_WALLETCONNECT_ID || ''
-
-
-// declare function configureChains<TChain extends Chain = Chain>(defaultChains: TChain[], providers: ChainProviderFn<TChain>[], { batch, pollingInterval, rank, retryCount, retryDelay, stallTimeout, }?: ConfigureChainsConfig): {
-//     readonly chains: TChain[];
-//     readonly publicClient: ({ chainId }: {
-//         chainId?: number | undefined;
-//     }) => PublicClient<FallbackTransport>;
-//     readonly webSocketPublicClient: ({ chainId }: {
-//         chainId?: number | undefined;
-//     }) => WebSocketPublicClient<FallbackTransport> | undefined;
-// };
 
 const { chains, publicClient } = configureChains(
     [gnosis],
